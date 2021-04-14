938f2bb3890806cefc2af847cf13f98c522a0090
pama-ibm <pama@ibm.com>
2021-04-14 13:49:06 -0400

    remove unneeded comment

--- config/cicd-slack/test/env.sh
+++ config/cicd-slack/test/env.sh
@@ -2,8 +2,4 @@
 ### PARAMETERS ###
 ##################
 
-# Username and Password for an OpenShift user with cluster-admin privileges.
-# cluster-admin privileges are required as this script deploys operators to
-# watch all namespaces.
-
 OCP_NAMESPACE=webhook-to-slack-pipeline
